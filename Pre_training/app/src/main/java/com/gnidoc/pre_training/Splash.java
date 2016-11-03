03c6a2050c8affc5c587e23df660392bd26c902c
han sung kim <han sung kim>
2016-11-03 22:46:00 +0900

    gps address update

--- Pre_training/app/src/main/java/com/gnidoc/pre_training/Splash.java
+++ Pre_training/app/src/main/java/com/gnidoc/pre_training/Splash.java
@@ -1,10 +1,21 @@
 package com.gnidoc.pre_training;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.PersistableBundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,12 +24,88 @@ import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.client.RestTemplate;
+
 public class Splash extends Activity {
     static final String TAG = Splash.class.getSimpleName();
+    static final String ACTION_GPS = "ACTION_GPS";
 
-    TextView start, notice, exercise;
+    TextView start, notice, exercise, gps;
     WebView webView;
 
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(ACTION_GPS)){
+                Log.i("BR", "get?");
+                gps.setText(intent.getStringExtra("address"));
+            }
+        }
+    };
+
+    LocationManager locationManager;
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            final double latitude = location.getLatitude();
+            final double longitude = location.getLongitude();
+
+            new AsyncTask<Void, Void, Void>() {
+                ProgressDialog progressDialog;
+                @Override
+                protected void onPreExecute() {
+                    progressDialog = ProgressDialog.show(Splash.this, "", "잠시만 기다려주세요");
+                    super.onPreExecute();
+                }
+
+                @Override
+                protected Void doInBackground(Void... voids) {
+                    String url1 = "https://apis.daum.net/local/geo/coord2addr?apikey=5ea7756fc938e1402f68f4520c439ec2&longitude=";
+                    String url2 = "&latitude=";
+                    String url3 = "&inputCoordSystem=WGS84&output=json";
+
+                    final RestTemplate restTemplate = new RestTemplate();
+                    final String result = restTemplate.getForObject(url1+longitude+url2+latitude+url3, String.class);
+                    Log.i("TH", result);
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(result);
+                        String address = jsonObject.getString("fullName");
+                        Intent intent = new Intent(ACTION_GPS);
+                        intent.putExtra("address", address);
+                        sendBroadcast(intent);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    progressDialog.dismiss();
+                    super.onPostExecute(aVoid);
+                }
+            }.execute();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     int count;
 
@@ -28,15 +115,52 @@ public class Splash extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+
         count = 0;
 
         start = (TextView) findViewById(R.id.start);
         notice = (TextView) findViewById(R.id.ex);
         exercise = (TextView) findViewById(R.id.notice);
+        gps = (TextView) findViewById(R.id.gps);
 
         webView = (WebView) findViewById(R.id.web_view);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    finish();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+        }
 
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     public void goToWiki(View v){
@@ -60,6 +184,15 @@ public class Splash extends Activity {
         webView.loadUrl("https://github.com/kimhs327/mobile_pre_training");
     }
 
+    public void shareSNS(View v){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "주제 - ");
+        intent.putExtra(Intent.EXTRA_TEXT, "인서야 잘하자");
+        Intent chooserIntent = Intent.createChooser(intent, "SNS");
+        startActivity(chooserIntent);
+    }
+
     @Override
     protected void onStart() {
         Log.i(TAG, "onStart");
@@ -77,12 +210,17 @@ public class Splash extends Activity {
         //TODO 화면을 닫을때마다 count 숫자를 증가시켜보자
         Log.i(TAG, "onResume, count = "+count);
         Toast.makeText(this, "인서야 잘하자", Toast.LENGTH_SHORT).show();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_GPS);
+
+        registerReceiver(broadcastReceiver, intentFilter);
         super.onResume();
     }
 
     @Override
     protected void onPause() {
         Log.i(TAG, "onPause");
+        unregisterReceiver(broadcastReceiver);
         super.onPause();
     }
 
@@ -97,4 +235,6 @@ public class Splash extends Activity {
         Log.i(TAG, "onDestroy");
         super.onDestroy();
     }
+
+
 }
